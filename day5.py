34ecf453c2ac4ce242f235d33448f0b370d95ca6
mcmathmo <75301986+mcmathmo@users.noreply.github.com>
2020-12-05 12:37:03 +0000

    Introduce main() function
    Style change and refactor. Introduce main() function

--- day5.py
+++ day5.py
@@ -18,7 +18,7 @@ def find_missing(ids):
             return(ids[i]-1)
 
 
-def masterfunc(line_list, part):
+def get_result(line_list, part):
     if part == 1:
         return max([get_id(line) for line in line_list])
     if part == 2:
@@ -31,20 +31,24 @@ def test(day, targetvals):
         line_list = read_data.split('\n')
     resultvals = [0 for _ in range(len(targetvals))]
     for i, _ in enumerate(targetvals):
-        resultvals[i] = masterfunc(line_list, i+1)
+        resultvals[i] = get_result(line_list, i+1)
         if resultvals[i] != testvals[i]:
             print(f'Test part {i+1} fails: expected '
                   f'{testvals[i]} but got {resultvals[i]}')
     return resultvals == testvals
 
 
-if __name__ == "__main__":
-    day = 5
-    testvals = [820, None]
+def main():
     if test(day, testvals):
         with open("day" + str(day) + "_input.txt") as input_file:
             read_data = input_file.read()
             line_list = read_data.split('\n')
         for i, _ in enumerate(testvals):
-            output = masterfunc(line_list, i+1)
+            output = get_result(line_list, i+1)
             print(f'Part {i+1}: {output}')
+
+
+if __name__ == "__main__":
+    day = 5
+    testvals = [820, None]
+    main()
